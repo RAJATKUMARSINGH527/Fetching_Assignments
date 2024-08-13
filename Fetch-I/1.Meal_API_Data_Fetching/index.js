eaac966bcfed56b519ab9bc4b94d69478634ee48
RAJATKUMARSINGH257 <rajatkumarsingh257@gmail.com>
2024-08-13 21:38:24 +0530

    Update index.html and index.js in Fetch-I/2.Fetching_and_Displaying_Users_from_Reqres_API

---
+++ Fetch-I/1.Meal_API_Data_Fetching/index.js
@@ -0,0 +1,25 @@
+const categoryBtn=document.getElementById("CategoryData");
+categoryBtn.addEventListener('click',getCategoryData);
+const ingrediantBtn=document.getElementById('IngredientData');
+//  add add event listner
+ingrediantBtn.addEventListener('click',getIngrediantData);
+// Function to fetch data for a specific category (e.g., Seafood)
+async function getCategoryData(){
+    try {
+        const response = await fetch('https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood');
+        const data = await response.json();
+        console.log(data);
+    }catch (error) {
+        console.log('Something went wrong',error);
+    }
+}
+// Function to fetch data for a specific ingredient (e.g., chicken_breast)
+async function getIngrediantData(){
+    try {
+        const response = await fetch('https://www.themealdb.com/api/json/v1/1/filter.php?i=chicken_breast');
+        const data = await response.json();
+        console.log(data);
+    }catch (error) {
+        console.log('Something went wrong',error);
+    }
+}
